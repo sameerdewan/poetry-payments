46936e6fc15a3e598f9fad308f935bde364737d8
sameerdewan <sameer.dewan@itential.com>
2021-03-10 23:09:41 -0500

    redo

--- db/models/User.js
+++ db/models/User.js
@@ -38,38 +38,6 @@ const userSchema = new mongoose.Schema({
     }
 });
 
-userSchema.methods.createCustomer = async function(stripeToken) {
-    const customer = await stripe.customers.create({
-        email: this.email,
-        source: stripeToken
-    });
-    await mongoose.model('User').findOneAndUpdate(
-        { username: this.username },
-        { $set: { customerId: customer.id } }
-    ).exec();
-}
-
-userSchema.methods.chargeCustomer = async function({ description, amount, statement_descriptor_suffix }) {
-    const charge = await stripe.charges.create({
-        amount,
-        description,
-        statement_descriptor_suffix,
-        currency: 'usd',
-        customer: this.customerId,
-        receipt_email: this.email,
-        metadata: {
-            username: this.username,
-            email: this.email,
-            customerId: this.customerId
-        }
-    });
-    await mongoose.model('User').findOneAndUpdate(
-        { username: this.username, customId: this.customerId },
-        { $set: { credit: amount } }
-    ).exec();
-    return charge;
-}
-
 const User = mongoose.model('User', userSchema);
 
 module.exports = User;
