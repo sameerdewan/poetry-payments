8e9a75d4b1e64d8befe9371bc08df68c8911d8e4
sameerdewan <sameer.dewan@itential.com>
2021-03-11 12:29:07 -0500

    add subscription route

--- api/app.js
+++ api/app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
 const customers = require('../routes/customers');
+const subscriptions = require('../routes/subscriptions');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use('/api/customers', customers);
+app.use('/api/subscriptions', subscriptions);
 
 module.exports = app;
