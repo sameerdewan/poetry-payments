b3c9b90262ad87676b5089d1f09af20571c081ed
sameerdewan <sameer.dewan@itential.com>
2021-03-10 21:34:53 -0500

    user model w stripe integration

---
+++ db/models/User.js
@@ -0,0 +1,71 @@
+const mongoose = require('mongoose');
+require('mongoose-type-email');
+const stripe = require('stripe')(process.env.STRIPE_KEY);
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: mongoose.SchemaTypes.String,
+        unique: true,
+        required: true,
+        min: [3, 'Username is too short'],
+        max: [20, 'Username is too long']
+    },
+    password: {
+        type: mongoose.SchemaTypes.String,
+        required: true,
+        min: [5, 'Password is too short']
+    },
+    email: {
+        type: mongoose.SchemaTypes.Email,
+        unique: true,
+        required: true
+    },
+    validated: {
+        type: mongoose.SchemaTypes.Boolean,
+        default: false
+    },
+    validationCode: {
+        type: mongoose.SchemaTypes.String,
+        required: true
+    },
+    credit: {
+        type: mongoose.SchemaTypes.Number,
+        default: 0
+    },
+    customerId: {
+        type: mongoose.SchemaTypes.String,
+        default: null
+    }
+});
+
+userSchema.methods.createCustomer = async function(stripeToken) {
+    const stripeObj = {
+        email: this.email,
+        source: stripeToken
+    };
+    const customer = await stripe.customers.create(stripeObj);
+    await mongoose.model('User').findOneAndUpdate(
+        { username: this.username },
+        { $set: { customerId: customer.id } }
+    ).exec();
+}
+
+userSchema.methods.chargeCustomer = async function({ description, amount, statementMsg }) {
+    const charge = await stripe.charges.create({
+        amount,
+        description,
+        statement_descriptor_suffix: statementMsg,
+        currency: 'usd',
+        customer: this.customerId,
+        receipt_email: this.email,
+    });
+    await mongoose.model('User').findOneAndUpdate(
+        { username: this.username, customId: this.customerId },
+        { $set: { credit: amount } }
+    ).exec();
+    return charge;
+}
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
---
+++ routes/paymentGateway.js
@@ -0,0 +1,14 @@
+const router = require('express').Router();
+const PoetrySystemJWT = require('../jwt');
+
+const apiSpec = path.join(__dirname, 'paymentGateway.yaml');
+const poetryJWT = new PoetrySystemJWT();
+
+router.use(
+    OpenApiValidator.middleware({
+        apiSpec,
+        validateRequests: true,
+    })
+);
+
+module.exports = router;
